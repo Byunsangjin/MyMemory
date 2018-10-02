02c5685fd90156fc71dabf2be382ca4813111c74
byunsangjin <qustkdwls1@naver.com>
2018-10-03 00:19:48 +0900

    사이드 바 구현

--- MyMemory/MyMemory/MemoListVC.swift
+++ MyMemory/MyMemory/MemoListVC.swift
@@ -16,6 +16,28 @@ class MemoListVC: UITableViewController {
         return count
     }
     
+    
+    
+    override func viewDidLoad() {
+        // SWRevealViewController 라이브러리의 revealViewController 객체를 읽어온다.
+        if let revealVC = self.revealViewController() {
+            
+            // 바 버튼 아이템 객체를 정의한다.
+            let btn = UIBarButtonItem()
+            btn.image = UIImage(named: "sidemenu.png") // 이미지는 sidemenu.png
+            btn.target = revealVC // 버튼 클릭 시 호출할 메소드가 정의된 객체를 지정
+            btn.action = #selector(revealVC.revealToggle(_:)) // 버튼 클릭 시 revealToggle(_:) 호출
+            
+            // 정의된 바 버튼을 내비게이션 바의 왼쪽 아이템에 등록한다.
+            self.navigationItem.leftBarButtonItem = btn
+            
+            // 제스처 객체를 뷰에 추가한다.
+            self.view.addGestureRecognizer(revealVC.panGestureRecognizer())
+        }
+    }
+    
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // memoList 배열 데이터에서 주어진 행에 맞는 데이터를 꺼낸다.
         let row = self.appDelegate.memoList[indexPath.row]
